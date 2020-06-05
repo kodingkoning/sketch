1604d9cb56de3db09c448d8261cb65fc00f811b5
Elizabeth Koning <koningelizabeth@gmail.com>
2020-06-05 15:15:17 -0700

    cori job used for graph

--- cori_job.sh
+++ cori_job.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#BATCH -N 4
+#BATCH -N 1
 #SBATCH -C haswell
 #SBATCH -q debug
 #SBATCH -J caldisks
-#SBATCH -o caldisks.%j.stdout
-#SBATCH -e caldisks.%j.error
-#SBATCH --mail-user=erk24@berkeley.edu
-#SBATCH --mail-type=ALL
+#SBATCH -o final/caldisks.%j.stdout
+#SBATCH -e final/caldisks.%j.error
 #SBATCH -t 30:00
 
 module swap PrgEnv-intel PrgEnv-gnu
@@ -16,5 +14,30 @@ cd include/sketch/BigInt/
 make
 cd ../../../
 ./run_tests.sh 
-mpirun -np 64 caldiskstest celegans500.fastq
 
+# mpirun -np  1 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+# mpirun -np  2 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+# mpirun -np  4 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+mpirun -np  8 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+mpirun -np 16 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+mpirun -np 32 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+
+# mpirun -np 1 caldiskstest 7 50 test_a.txt test_b.txt
+# mpirun -np 2 caldiskstest 7 50 test_a.txt test_b.txt
+# mpirun -np 4 caldiskstest 7 50 test_a.txt test_b.txt
+# mpirun -np 8 caldiskstest 7 50 test_a.txt test_b.txt
+# mpirun -np 16 caldiskstest 7 50 test_a.txt test_b.txt
+
+
+# mpirun -np 1 caldiskstest 21 150 celegans_e.fastq ecsample4000.fastq
+# mpirun -np 2 caldiskstest 21 150 celegans_e.fastq ecsample4000.fastq
+# mpirun -np 4 caldiskstest 21 150 celegans_e.fastq ecsample4000.fastq
+
+
+#mpirun -np 1 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+# mpirun -np 4 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+#mpirun -np 8 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+#mpirun -np 16 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+#mpirun -np 4 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq
+#mpirun -np 5 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq
+#mpirun -np 1 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
