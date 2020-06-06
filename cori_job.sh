a968d626d6fe4bd2feb320bf178d49ba209e0e1f
Elizabeth Koning <koningelizabeth@gmail.com>
2020-06-06 11:53:25 -0700

    cori slurm file with parallel I/O

--- cori_job.sh
+++ cori_job.sh
@@ -1,43 +1,38 @@
 #!/bin/bash
-#BATCH -N 1
+#SBATCH -N 8
+#SBATCH --tasks-per-node=32
 #SBATCH -C haswell
 #SBATCH -q debug
 #SBATCH -J caldisks
 #SBATCH -o final/caldisks.%j.stdout
 #SBATCH -e final/caldisks.%j.error
-#SBATCH -t 30:00
+#SBATCH -t 7:00
+#DW jobdw capacity=10GB access_mode=striped type=scratch
+#DW stage_in source=/global/cscratch1/sd/erk24/data/real-data/celegans40x_allfastq.fastq destination=$DW_JOB_STRIPED/celegans40x_allfastq.fastq type=file
+#DW stage_in source=/global/cscratch1/sd/erk24/data/real-data/ecfull100x.fastq destination=$DW_JOB_STRIPED/ecfull100x.fastq type=file
+
+# NOTE: the files must be located in the scratch buffer to work with Cori's burst buffer. This enables parallel I/O
+# More info about the Burst buffer and scratch buffer here: https://www.nersc.gov/assets/Uploads/Burst-Buffer-tutorial.pdf
+# The capacity requested should coordinate with the size of the files
+
+echo "nodes =" $SLURM_JOB_NUM_NODES
+echo "tasks/node =" $SLURM_NTASKS_PER_NODE
+echo "Using k = 13 and sketch size = 150"
+echo
+
+TASKS=$(($SLURM_JOB_NUM_NODES*$SLURM_NTASKS_PER_NODE))
+
+FILE_A=${DW_JOB_STRIPED}celegans40x_allfastq.fastq
+FILE_B=${DW_JOB_STRIPED}ecfull100x.fastq
 
 module swap PrgEnv-intel PrgEnv-gnu
 module load openmpi
-rm caldiskstest
+# rm caldiskstest # caldiskstest does not rebuild properly if not deleted. This should be uncommented during development
 cd include/sketch/BigInt/
 make
 cd ../../../
 ./run_tests.sh 
 
-# mpirun -np  1 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
-# mpirun -np  2 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
-# mpirun -np  4 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
-mpirun -np  8 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
-mpirun -np 16 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
-mpirun -np 32 caldiskstest 13 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
-
-# mpirun -np 1 caldiskstest 7 50 test_a.txt test_b.txt
-# mpirun -np 2 caldiskstest 7 50 test_a.txt test_b.txt
-# mpirun -np 4 caldiskstest 7 50 test_a.txt test_b.txt
-# mpirun -np 8 caldiskstest 7 50 test_a.txt test_b.txt
-# mpirun -np 16 caldiskstest 7 50 test_a.txt test_b.txt
-
-
-# mpirun -np 1 caldiskstest 21 150 celegans_e.fastq ecsample4000.fastq
-# mpirun -np 2 caldiskstest 21 150 celegans_e.fastq ecsample4000.fastq
-# mpirun -np 4 caldiskstest 21 150 celegans_e.fastq ecsample4000.fastq
-
-
-#mpirun -np 1 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
-# mpirun -np 4 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
-#mpirun -np 8 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
-#mpirun -np 16 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
-#mpirun -np 4 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq
-#mpirun -np 5 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq
-#mpirun -np 1 caldiskstest 21 150 /project/projectdirs/m1982/bella-data/real-data/celegans40x_allfastq.fastq /project/projectdirs/m1982/bella-data/real-data/ecfull100x.fastq
+mpirun -np $TASKS caldiskstest 13 150 $FILE_A $FILE_B
+mpirun -np $TASKS caldiskstest 13 150 $FILE_A $FILE_B
+mpirun -np $TASKS caldiskstest 13 150 $FILE_A $FILE_B
